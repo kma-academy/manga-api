7bd8861e36c540f5d30da58e854cd593b1838ef5
Cuong Tran <clonebmn2itt@gmail.com>
2020-10-24 14:15:22 +0700

    chore: Home router

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const { PORT = 8080 } = process.env;
-
+app.get("/", function (req, res) {
+    res.send("Hello")
+})
 app.use("/mangas", require('./app/routes/mangas'));
 
 app.listen(PORT, function () {
